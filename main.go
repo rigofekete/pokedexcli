696cabcc973d1407323f91ffd6aec9700ed15917
rigofekete <fabricecosteira@gmail.com>
2025-09-15 17:21:09 +0200

    first repl cli logic

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 		"fmt"
+		"bufio"
+		"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	for { 
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("\nPokedex > ")
+
+		var input string
+
+		scanner.Scan()
+		input = scanner.Text()
+
+		final_input := cleanInput(input)
+
+		fmt.Printf("Your command was: %s", final_input[0])
+	}
 }
+
 
 
 
